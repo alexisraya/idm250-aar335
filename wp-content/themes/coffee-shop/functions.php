95c2b29267a0bd7cf5f9df1d1fcb8d941d400716
Alexis Raya <aar335@drexel.edu>
2023-01-31 19:59:28 -0500

    :construction: week 4 class updates

--- wp-content/themes/coffee-shop/functions.php
+++ wp-content/themes/coffee-shop/functions.php
@@ -1,1 +1,24 @@
 <?php
+
+function theme_scripts_and_styles(){
+     // Load in local CSS {@link https://developer.wordpress.org/reference/functions/wp_enqueue_style/}
+     wp_enqueue_style(
+        'idm250-styles', // name of the stylesheet
+        get_template_directory_uri() . '/dist/styles/main.css', // http://localhost:250/wp-content/themes/idm250-theme-02/dist/styles/main.css
+        [], // dependencies
+        filemtime(get_template_directory() . '/dist/styles/main.css'), // version number
+        'all' // media
+    );
+
+    // Load in local JS {@link https://developer.wordpress.org/reference/functions/wp_enqueue_script/}
+    wp_enqueue_script(
+        'idm250-scripts', // name of the script
+        get_template_directory_uri() . '/dist/scripts/main.js', // http://localhost:250/wp-content/themes/idm250-theme-02/dist/scripts/main.js
+        [], // dependencies
+        filemtime(get_template_directory() . '/dist/scripts/main.js'), // version number
+        true // load in footer
+    );
+
+}
+
+add_action('wp_enqueue_scripts', 'theme_scripts_and_styles');
---
+++ wp-content/themes/coffee-shop/page.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+
+this is the page.php
+
+<?php get_footer();?>
