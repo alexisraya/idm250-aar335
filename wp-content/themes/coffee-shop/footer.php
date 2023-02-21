23801c9f9c52b95509ad2e625d330d5a7cba0a6a
Alexis Raya <aar335@drexel.edu>
2023-02-20 20:36:32 -0500

    :sparkles: update footer style

--- wp-content/themes/coffee-shop/footer.php
+++ wp-content/themes/coffee-shop/footer.php
@@ -1,3 +1,10 @@
-</body>
+</main>
+<footer>
+  <img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/dist/images/Logo.png" alt="Coffee Shop Logo" />
+  <p>The Coffee House &copy; <?php echo date('Y'); ?></p>
+  <!-- <?php wp_nav_menu(['theme_location' => 'footer-menu']); ?> -->
+</footer>
 <?php wp_footer(); ?>
+</body>
+
 </html>
