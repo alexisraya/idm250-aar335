5f82461497df77b4ff912ecc097043a12ed02e41
Alexis Raya <aar335@drexel.edu>
2023-03-23 23:55:19 -0400

    tester

--- wp-content/themes/coffee-shop/single-coffees.php
+++ wp-content/themes/coffee-shop/single-coffees.php
@@ -1,6 +1,7 @@
 <?php
     get_header();
 ?>
+<h1> hello </h1>
 <h1><?php echo get_the_title(); ?></h1>
 <div class="my-single-shoes-images-div">
   <img class="my-single-shoes-images" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title(); ?> Featured Image">
